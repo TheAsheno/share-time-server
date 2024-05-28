a249235b5be9581b16a9acef39c9259c07a09495
TheAsheno <1296940185@qq.com>
2024-05-28 14:21:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,26 +7,25 @@ const app = express();
 
 app.use(cors());
 
-// 让 Node 为我们创建的 React 应用提供文件
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
-// 处理对/api 的 GET 请求
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!", info: "This is a message from the server."});
+  res.json({ message: "Hello from server!", info: "This is a message from the server." });
 });
 
-app.get("/api/music", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'musicName.json'));
+app.get("/api/musiclist", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/dicts/MusicList.json'));
 });
 
-app.get("/musicName.json", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'musicName.json'));
+app.use('/music/audios', (req, res, next) => {
+  if (req.path === '/') {
+    res.json({ message: 'Welcome to audios!' });
+  } else {
+    next();
+  }
 });
 
-// 所有之前未被处理的 GET 请求将返回我们的 React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
-});
+app.use('/music', express.static('public/music'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
